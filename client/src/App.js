3749077d533f69d64c69455bb4cbb1fda717ed27
Colette Ulianogit config --global --listclear <culiano@live.com>
2023-04-14 09:11:28 -0400

    HeaderNav component finished

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import "./App.css";
 
 function App() {
     return (
-        <div className='container'>
+        <div>
             <HeaderNav />
             
             <Routes>
--- client/src/components/HeaderNav.jsx
+++ client/src/components/HeaderNav.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
+import logo from '../assets/logo-slate.png';
 import { Link } from 'react-router-dom';
 
 const HeaderNav = () => {
     return (
         <>
-            <header className="border-bottom border-lg-0 border-primary">
+            <header className="border-bottom border-primary">
                 <nav className="navbar navbar-light navbar-expand-lg" aria-label="">
                     <div className="container">
-                        <a className="navbar-brand" href="#">
-                            <img src="assets/logo-slate.png" alt="" />
-                        </a>
+                        <Link className="navbar-brand" to="/">
+                            <img src={logo} alt="" />
+                        </Link>
                         <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#my-navbar" aria-controls="" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="my-navbar">
                             <ul className="navbar-nav ms-auto text-center mb-2 mb-lg-0">
                                 <li className="nav-item ms-lg-3">
-                                    <a className="nav-link" href="/"><i className="bi bi-house-door-fill"></i> Home</a>
+                                    <Link className="nav-link" to="/"><i className="bi bi-house-door-fill"></i> Home</Link>
                                 </li>
                                 <li className="nav-item ms-lg-3">
-                                    <a className="nav-link" href="/about"><i className="bi bi-person-fill"></i> About</a>
+                                    <Link className="nav-link" to="/about"><i className="bi bi-person-fill"></i> About</Link>
                                 </li>
                                 <li className="nav-item ms-lg-3">
-                                    <a className="nav-link" href="/post"><i className="bi bi-linkedin"></i> Posts</a>
+                                    <Link className="nav-link" to="/post"><i className="bi bi-pencil-square"></i> Posts</Link>
                                 </li>
                                 <li className="nav-item ms-lg-3">
                                     <a className="nav-link" href="https://www.linkedin.com/in/culiano/" target="_blank"><i className="bi bi-linkedin"></i> LinkedIn</a>
